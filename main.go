c770735c9bb27205e750e755780bf97d52f06726
ajaykumarguptaa <theincredibleknowledge@gmail.com>
2025-07-04 08:57:07 +0530

    Initial commit

--- main.go
+++ main.go
@@ -1,30 +1,14 @@
-// package main
-
-// import (
-// 	"fmt"
-
-// 	"github.com/gofiber/fiber/v2"
-// )
-
-// func main() {
-// 	fmt.Print("hello world!")
-// 	app := fiber.New()
-// 	app.Get("/abc", func(c *fiber.Ctx) error {
-// 		return c.SendString("Hello Ajay!\n hello")
-
-// 	})
-// 	app.Post("/sum", )
-// 	app.Listen(":3000")
-// }
-
-
-
-	package main
+package main
 
 import (
 	"fmt"
-
 	"github.com/gofiber/fiber/v2"
+)
+import (
+	"database/sql"
+	_ "github.com/lib/pq" // PostgreSQL driver
+	"log"
+	"time"
 )
 
 func main() {
@@ -36,6 +20,10 @@ func main() {
 	})
 	app.Post("/sum", AddOperation)
 	app.Listen(":3000")
+
+	if err := CreateDbObject(); err != nil {
+		log.Fatal("Database connection failed:", err)
+	}
 
 }
 
@@ -50,7 +38,7 @@ func AddOperation(c *fiber.Ctx) error {
 	result := request.Num1 + request.Num2
 	response := AddOperationResponse{Result: result}
 
-	return c.JSON(response) 
+	return c.JSON(response)
 }
 
 type AddOperationRequest struct {
@@ -61,4 +49,43 @@ type AddOperationRequest struct {
 type AddOperationResponse struct {
 	Result int `json:"result"`
 }
-	
+
+var DB *sql.DB
+
+const (
+	HOST     = "dpg-d1ibu62li9vc73fpqmmg-a"
+	PORT     = "5432"
+	USERNAME = "ajay_k045_user"
+	PASSWORD = "JqfFofPysrAIp8aPst8OQLq8yQNQW5d3"
+	DBNAME   = "Ajay"
+)
+
+func GetPsqlInfo() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
+		HOST, PORT, USERNAME, PASSWORD, DBNAME)
+}
+
+func CreateDbObject() error {
+	var err error
+
+	DB, err = sql.Open("postgres", GetPsqlInfo())
+	if err != nil {
+		return fmt.Errorf("error opening DB: %w", err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		return fmt.Errorf("error connecting to DB: %w", err)
+	}
+
+	fmt.Println("Connected successfully")
+
+	DB.SetMaxOpenConns(25)
+	DB.SetMaxIdleConns(25)
+	DB.SetConnMaxIdleTime(10 * time.Minute)
+	DB.SetConnMaxLifetime(1 * time.Hour)
+
+	return nil
+}
+
+// Continue with your logic here
